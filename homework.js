6b5da224186d56dad96b5e06f2e95afcc8b34ae2
Kari Scharf <karischarf@gmail.com>
2018-08-07 15:58:18 -0400

    working on problem 3

--- homework.js
+++ homework.js
@@ -1,18 +1,50 @@
 // 1. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
 
-function sum (numbers) {
-  console.log('numbers', numbers)
-  return 0
+
+function sum(numbers) {
+  var total = 0
+
+  for (var i = 0; i < numbers.length; i++) {
+    total += numbers[i]
 }
+    return total 
+}
+                       
+      
 
 // 2. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(numbers) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i] 
+  }
+    if (numbers.length <= 0) {
+    return undefined;
+    }
+  return sum / numbers.length;
+}
 
 // 3. Create a function called "intersection" that takes two arrays and
 // returns a new array that contains the elements found in both arrays.
 // The order they are in does not matter, but no duplicates should be
 // in the returned array, even if they were in the input.
+function intersection (array1,array2) {
+   var dupes = []
+
+for (var num1 of array1) {
+  for (var num2 of array2) {
+  }
+   if (num1 === num2 dupes.push(num1) {
+    return  
+   }
+}
+  
+
+
+  
+
 
 // 4. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
