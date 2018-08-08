ac91965b92a82ed5d0209aa2cd6d4ee1f500495a
Kari Scharf <karischarf@gmail.com>
2018-08-08 09:56:30 -0400

    solved problem 6

--- homework.js
+++ homework.js
@@ -56,56 +56,55 @@ function minimum(nums) {
   return min
 }
 
-  // function minimum(numbers) {
-  //   var min = Math.min.apply(null,numbers);
-  //   numbers.splice(numbers.indexOf(min),1);
-  //   return numbers;
-  // }
 
 
+// 5. There are many techniques to sort arrays in programming. Your programming
+// language will likely include the ability to do this. We are going to
+// implement sorting ourselves, however.
+//
+// A "selection sort" is one of the most simple sorting algorithms. To implement it,
+// you start with an unsorted array of numbers. You search the array and find the
+// smallest number in the array. You then insert that into a new array as the first
+// element and remove it from the original array. You continue doing this until
+// there are no numbers left in the original array.
+//
+// Create a function called selectionSort that takes an array of numbers and returns
+// a sorted array using the above technique.
+//
+// Note 1: You do not actually want to delete things from the original array. You
+// should copy it first. To copy an array, use the following code:
+//
+// var arrayCopy = array.slice(0);
+//
+// Think about why this works.
+//
+// Note 2: Selection sort can be implemented using one array. Read the explanation at
+// https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
+// to see how. This may make more sense to you.
 
+// 6. Create a function called "createUser" that takes a name and a Date object
+// and returns an object with the keys "name" and "dob" (date of birth) with
+// those values.
+function createUser(name, dateOfBirth) {
+  return {
+    name: name,
+    dob: dateOfBirth
+  }
+}
 
-  // 5. There are many techniques to sort arrays in programming. Your programming
-  // language will likely include the ability to do this. We are going to
-  // implement sorting ourselves, however.
-  //
-  // A "selection sort" is one of the most simple sorting algorithms. To implement it,
-  // you start with an unsorted array of numbers. You search the array and find the
-  // smallest number in the array. You then insert that into a new array as the first
-  // element and remove it from the original array. You continue doing this until
-  // there are no numbers left in the original array.
-  //
-  // Create a function called selectionSort that takes an array of numbers and returns
-  // a sorted array using the above technique.
-  //
-  // Note 1: You do not actually want to delete things from the original array. You
-  // should copy it first. To copy an array, use the following code:
-  //
-  // var arrayCopy = array.slice(0);
-  //
-  // Think about why this works.
-  //
-  // Note 2: Selection sort can be implemented using one array. Read the explanation at
-  // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
-  // to see how. This may make more sense to you.
+// 7. Create a function called "calculateAge" that takes a user created from
+// createUser and a Date object considered the current date, and calculates the user's
+// age in years on that date. You can use your code from yesterday's homework.
 
-  // 6. Create a function called "createUser" that takes a name and a Date object
-  // and returns an object with the keys "name" and "dob" (date of birth) with
-  // those values.
+// 8. Create a function called "addAge" that takes a user created from createUser
+// and a Date object and adds a new key on the user object, "age", with the age
+// in years the user was on that date.
 
-  // 7. Create a function called "calculateAge" that takes a user created from
-  // createUser and a Date object considered the current date, and calculates the user's
-  // age in years on that date. You can use your code from yesterday's homework.
+// 9. Create a function called "createUsers" that takes two arrays of equal
+// length, the first being a list of names and the second being a list of dates of
+// birth, and returns a new array of objects created from those original arrays.
 
-  // 8. Create a function called "addAge" that takes a user created from createUser
-  // and a Date object and adds a new key on the user object, "age", with the age
-  // in years the user was on that date.
-
-  // 9. Create a function called "createUsers" that takes two arrays of equal
-  // length, the first being a list of names and the second being a list of dates of
-  // birth, and returns a new array of objects created from those original arrays.
-
-  // 10. Create a function called "averageAge" that takes an array of users and
-  // a Date object and returns the average age in years of the users on that date.
-  // You do not have to handle the situation in which the current date is before
-  // a user's date of birth.
+// 10. Create a function called "averageAge" that takes an array of users and
+// a Date object and returns the average age in years of the users on that date.
+// You do not have to handle the situation in which the current date is before
+// a user's date of birth.
